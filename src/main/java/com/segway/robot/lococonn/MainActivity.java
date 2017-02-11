a2f8125e5e273feb7994d72b3d3ba417d96c6fef
deepGem <pradeepbs@gmail.com>
2017-02-11 15:20:09 +0800

    Working copy of locomotion and message receiving for the Loomo robot

---
+++ src/main/java/com/segway/robot/lococonn/MainActivity.java
@@ -0,0 +1,215 @@
+package com.segway.robot.lococonn;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+import android.util.Log;
+
+import android.widget.Toast;
+
+import com.segway.robot.sdk.base.bind.ServiceBinder;
+
+import com.segway.robot.sdk.baseconnectivity.Message;
+import com.segway.robot.sdk.baseconnectivity.MessageConnection;
+import com.segway.robot.sdk.baseconnectivity.MessageRouter;
+import com.segway.robot.sdk.connectivity.RobotException;
+import com.segway.robot.sdk.connectivity.RobotMessageRouter;
+import com.segway.robot.sdk.connectivity.StringMessage;
+
+import com.segway.robot.sdk.locomotion.sbv.AngularVelocity;
+import com.segway.robot.sdk.locomotion.sbv.Base;
+import com.segway.robot.sdk.locomotion.sbv.LinearVelocity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends Activity {
+    private boolean isBind = false;
+    private static final String TAG = "RobotActivity";
+    private int press = 0;
+    private RobotMessageRouter mRobotMessageRouter = null;
+    private MessageConnection mMessageConnection = null;
+    Base mBase;
+    private static final int RUN_TIME = 2000;
+    private Timer mTimer;
+
+    private ServiceBinder.BindStateListener mBindStateListener = new ServiceBinder.BindStateListener() {
+        @Override
+        public void onBind() {
+            Log.d(TAG, "onBind: ");
+            try {
+                //register MessageConnectionListener in the RobotMessageRouter
+                mRobotMessageRouter.register(mMessageConnectionListener);
+            } catch (RobotException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onUnbind(String reason) {
+            Log.e(TAG, "onUnbind: " + reason);
+        }
+    };
+
+    private MessageRouter.MessageConnectionListener mMessageConnectionListener = new RobotMessageRouter.MessageConnectionListener() {
+        @Override
+        public void onConnectionCreated(final MessageConnection connection) {
+            Log.d(TAG, "onConnectionCreated: " + connection.getName());
+            mMessageConnection = connection;
+            try {
+                mMessageConnection.setListeners(mConnectionStateListener, mMessageListener);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    };
+
+    private MessageConnection.ConnectionStateListener mConnectionStateListener = new MessageConnection.ConnectionStateListener() {
+        @Override
+        public void onOpened() {
+            Log.d(TAG, "onOpened: ");
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(), "connected to: " + mMessageConnection.getName(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public void onClosed(String error) {
+            Log.e(TAG, "onClosed: " + error);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(), "disconnected to: " + mMessageConnection.getName(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    };
+
+    private MessageConnection.MessageListener mMessageListener = new MessageConnection.MessageListener() {
+        @Override
+        public void onMessageSentError(Message message, String error) {
+
+        }
+
+        @Override
+        public void onMessageSent(Message message) {
+            Log.d(TAG, "onBufferMessageSent: id=" + message.getId() + ";timestamp=" + message.getTimestamp());
+        }
+
+        @Override
+        public void onMessageReceived(final Message message) {
+            Log.d(TAG, "onMessageReceived: id=" + message.getId() + ";timestamp=" + message.getTimestamp());
+            if (message instanceof StringMessage) {
+                //Hard coded robot value here to start the navigation activity
+                if (message.equals("Go")) {
+                   if(isBind) {
+                       // Add code to grab medicine
+                       
+                       moveRobot();
+                   }
+
+                }
+
+            } else {
+                //message received is BufferMessage
+                byte[] bytes = (byte[]) message.getContent();
+                Log.d(TAG, "onMessageReceived: file name=" + bytes);
+
+            }
+        }
+    };
+
+    private void moveRobot() {
+        new Thread() {
+            //@Override
+         public void run() {
+                float mLinearVelocity = 0.5f;
+                if (isBind) {
+                    mBase.setLinearVelocity(mLinearVelocity);
+                    mBase.setAngularVelocity(0.2f);
+                }
+                // set robot base linearVelocity, unit is rad/s, rand is -PI ~ PI.
+
+
+                // let the robot run for 2 seconds
+                try {
+                    Thread.sleep(RUN_TIME);
+                } catch (InterruptedException e) {
+                }
+
+                // stop
+                if (isBind) {
+                    mBase.setLinearVelocity(0);
+                }
+
+         }
+      }.start();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        initBase();
+        //get RobotMessageRouter
+        mRobotMessageRouter = RobotMessageRouter.getInstance();
+        //bind to connection service in robot
+        mRobotMessageRouter.bindService(this, mBindStateListener);
+
+        //moveRobot();
+    }
+
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        try {
+            mRobotMessageRouter.unregister();
+        } catch (RobotException e) {
+            e.printStackTrace();
+        }
+        mRobotMessageRouter.unbindService();
+        Log.d(TAG, "onDestroy: ");
+
+    }
+
+    private void initBase() {
+        // get Base Instance
+        mBase = Base.getInstance();
+        // bindService, if not, all Base api will not work.
+        mBase.bindService(getApplicationContext(), new ServiceBinder.BindStateListener() {
+            @Override
+            public void onBind() {
+                isBind = true;
+                mTimer = new Timer();
+                mTimer.scheduleAtFixedRate(new TimerTask() {
+                    @Override
+                    public void run() {
+                        final AngularVelocity av = mBase.getAngularVelocity();
+                        final LinearVelocity lv = mBase.getLinearVelocity();
+                        Log.d("Linear Velocity ::" , lv.toString());
+                        moveRobot();
+                    }
+                }, 50, 200);
+            }
+
+            @Override
+            public void onUnbind(String reason) {
+                isBind = false;
+                if (mTimer != null) {
+                    mTimer.cancel();
+                    mTimer = null;
+                }
+            }
+        });
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
---
+++ src/main/java/com/segway/robot/lococonn/Util.java
@@ -0,0 +1,34 @@
+package com.segway.robot.lococonn;
+
+import android.text.TextUtils;
+import android.widget.EditText;
+
+/**
+ * Created by sunguangshan on 2016/7/26.
+ */
+
+public class Util {
+    private static final String TAG = "Util";
+
+    public static boolean isEditTextEmpty(EditText editText) {
+        if (editText == null) {
+            throw new NullPointerException("editText is null");
+        }
+        String text = editText.getText().toString().trim();
+        if (TextUtils.isEmpty(text)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static String floatToString(float f) {
+        return String.valueOf(f);
+    }
+
+    public static float getEditTextFloatValue(EditText editText) {
+        String text = editText.getText().toString().trim();
+        return Float.parseFloat(text);
+    }
+
+}
